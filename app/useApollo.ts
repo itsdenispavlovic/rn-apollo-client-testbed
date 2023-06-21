71f9fc5848ba8e49bb4f15deb00e2eb3dc601d51
Denis Pavlovic <contact@denispavlovic.com>
2023-06-21 14:46:20 +0200

    Updated useApollo for React Native

--- app/useApollo.ts
+++ app/useApollo.ts
@@ -2,11 +2,16 @@ import { createUploadLink } from "apollo-upload-client";
 import { RetryLink } from "@apollo/client/link/retry";
 import { setContext } from "@apollo/client/link/context";
 import { ApolloClient, InMemoryCache, from } from "@apollo/client";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { CachePersistor, AsyncStorageWrapper } from "apollo3-cache-persist";
 
 interface Props {
   token: string | null;
   graphqlEndpoint: string;
 }
+
+const SCHEMA_VERSION = "4";
+const SCHEMA_VERSION_KEY = "apollo-schema-version";
 
 const authLink = (token: string | null) => {
   return setContext(async (_, { headers }) => {
@@ -21,6 +26,16 @@ const authLink = (token: string | null) => {
     return { headers };
   });
 };
+
+const cache = new InMemoryCache({
+  dataIdFromObject: (object: any) => {
+    switch (object.__typename) {
+      //User is whatever type "me" query resolves to
+      // case 'User': return object.name;
+      default: return object?.id || object?._id;
+    }
+  }
+});
 
 const retryLink = new RetryLink({
   delay: {
@@ -42,16 +57,42 @@ const retryLink = new RetryLink({
   },
 });
 
+async function setupPersistCache() {
+  const persistor = new CachePersistor({
+    cache,
+    storage: new AsyncStorageWrapper(AsyncStorage),
+    trigger: "write",
+    maxSize: false,
+    debug: false
+  });
+
+  // Read the current schema version from AsyncStorage.
+  const currentVersion = await AsyncStorage.getItem(SCHEMA_VERSION_KEY);
+
+  if (currentVersion === SCHEMA_VERSION) {
+    // If the current version matches the latest version,
+    // we're good to go and can restore the cache.
+    await persistor.restore();
+  } else {
+    // Otherwise, we'll want to purge the outdated persisted cache
+    // and mark ourselves as having updated to the latest version.
+    await persistor.purge();
+    await AsyncStorage.setItem(SCHEMA_VERSION_KEY, SCHEMA_VERSION);
+  }
+}
+
 export function createApolloClient({ token, graphqlEndpoint }: Props) {
+  setupPersistCache();
+
   return new ApolloClient({
-    cache: new InMemoryCache(),
+    cache,
     link: from([
       retryLink,
       authLink(token),
       createUploadLink({
-        uri: graphqlEndpoint || "http://localhost:1337/graphql",
-      }),
+        uri: graphqlEndpoint || "http://localhost:1337/graphql"
+      })
     ]),
-    connectToDevTools: process.env["NODE_ENV"] === "development",
+    connectToDevTools: process.env["NODE_ENV"] === "development"
   });
 }
